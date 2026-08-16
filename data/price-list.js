1c5de2f0a177613e9e67f288b0e1f4c3ecaa8794
27nemq-cyber <27nemq@gmail.com>
2026-08-16 12:01:53 +0200

    Create data/price-list.js

---
+++ data/price-list.js
@@ -0,0 +1,62 @@
+export const priceList = [
+  {
+    id: 1,
+    name: "Drážkovanie tehla / pórobetón",
+    unit: "bm",
+    price: 8
+  },
+  {
+    id: 2,
+    name: "Drážkovanie panel / betón",
+    unit: "bm",
+    price: 12
+  },
+  {
+    id: 3,
+    name: "Frézovanie otvoru pre zásuvku alebo vypínač",
+    unit: "ks",
+    price: 5
+  },
+  {
+    id: 4,
+    name: "Ručné vysekávanie drážky",
+    unit: "bm",
+    price: 7
+  },
+  {
+    id: 5,
+    name: "Osadenie elektroinštalačnej krabice",
+    unit: "ks",
+    price: 6
+  },
+  {
+    id: 6,
+    name: "Uloženie CYKY vodiča do drážky",
+    unit: "bm",
+    price: 3
+  },
+  {
+    id: 7,
+    name: "Uloženie kábla do lišty / trubky",
+    unit: "bm",
+    price: 3
+  },
+  {
+    id: 8,
+    name: "Zapojenie zásuvky alebo vypínača",
+    unit: "ks",
+    price: 9
+  },
+  {
+    id: 9,
+    name: "Zapojenie svietidla",
+    unit: "ks",
+    price: 12
+  },
+  {
+    id: 10,
+    name: "Montáž rozvodnice do 12 modulov",
+    unit: "ks",
+    price: 67
+  }
+];
